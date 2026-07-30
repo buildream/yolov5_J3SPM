d094eeb6c9c125376aa75a364a48ad8c979e2a71
buildream <51728374+buildream@users.noreply.github.com>
2026-07-30 14:51:11 +0900

    For fixed numpy and setuptools
    Signed-off-by: buildream <51728374+buildream@users.noreply.github.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -6,10 +6,10 @@ import os
 import warnings
 from pathlib import Path
 
-#import pkg_resources as pkg
+import pkg_resources as pkg
 #To handle updated setuptool
 
-from packaging.version import parse as parse_version
+#from packaging.version import parse as parse_version
 import torch
 
 from utils.general import LOGGER, colorstr, cv2
@@ -34,9 +34,9 @@ try:
     import wandb
 
     assert hasattr(wandb, "__version__")  # verify package import not local dir
-    #if pkg.parse_version(wandb.__version__) >= pkg.parse_version("0.12.2") and RANK in {0, -1}:
+    if pkg.parse_version(wandb.__version__) >= pkg.parse_version("0.12.2") and RANK in {0, -1}:
     #To handle updated setuptool
-    if parse_version(wandb.__version__) >= parse_version("0.12.2") and RANK in {0, -1}:
+    #if parse_version(wandb.__version__) >= parse_version("0.12.2") and RANK in {0, -1}:
 
         try:
             wandb_login_success = wandb.login(timeout=30)
